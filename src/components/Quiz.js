bebda7540c28dac6a6b26304b82a6135f52f49d1
Aleksandr <madness281@gmail.com>
2023-11-28 17:34:58 +0300

    работа с состояниями

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,12 +1,34 @@
 import Question from "./Question";
+import { useReducer } from "react";
 
+const initialState = {
+  question: [],
+  currentQuestionIndex: 0,
+};
+const reducer = (state, action) => {
+  console.log("reducer", state, action);
+  if (action.type === "NEXT_QESTION") {
+    return {
+      ...state,
+      currentQuestionIndex: state.currentQuestionIndex + 1,
+    };
+  }
+  return state;
+};
 const Quiz = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log("state", state);
   return (
     <div className="quiz ">
       <div>
         <div className="score">Question 1/8</div>
-        <Question />
-        <div className="next-button">Next Question</div>
+        <Question questions={state.questions} />
+        <div
+          className="next-button"
+          onClick={() => dispatch({ type: "NEXT_QESTION" })}
+        >
+          Next Question
+        </div>
       </div>
     </div>
   );
